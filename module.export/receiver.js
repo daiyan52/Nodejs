328d824f4323e290f3bfa2b4d5f4bc4855ec09ad
Daiyan Alam <daiyanalam@Daiyans-MacBook-Air.local>
2023-12-09 15:48:19 +0530

    updating

---
+++ module.export/receiver.js
@@ -0,0 +1,20 @@
+/*
+const value = require('./serder');
+console.log(value);
+*/
+
+const serder = require('./serder.js');
+
+
+console.log(serder.divide(4,2));
+
+/*
+console.log(serder); // { sum: [Function: sum], mul: [Function: mul], g: 9.8, PI: 3.14 }
+
+
+console.log(serder.sum(3,5));
+console.log(serder.mul(3,5));
+console.log(serder.PI);
+console.log(serder.g);
+
+*/
