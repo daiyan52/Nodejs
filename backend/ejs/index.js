06c67942779e5070a4a3490b350d3a70488540ac
Daiyan Alam <daiyanalam@Daiyans-MacBook-Air.local>
2023-12-25 16:11:03 +0530

    updating

--- backend/ejs/index.js
+++ backend/ejs/index.js
@@ -4,6 +4,9 @@ const app = express();
 
 const port = 8080;
 
+app.use(express.static(path.join(__dirname,"public")));
+
+
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 app.get('/',(req,res)=>{
@@ -15,4 +18,22 @@ app.get('/hello',(req,res)=>{
 })
 app.listen(port,()=>{
     console.log(`server is running on ${port}`)
+})
+
+app.get('/dice',(req,res)=>{
+    let diceValue  = Math.floor(Math.random()*6)+1
+    res.render('dice.ejs',{diceValue});
+})
+
+app.get('/ig/:username',(req,res)=>{
+    const instaData = require('./data.json');
+    let {username} = req.params;
+    let data = instaData[username];
+    // console.log(data);
+    if(data){
+        res.render('instragram.ejs',{data});
+    }
+    else{
+        res.render('error.ejs');
+    }
 })
